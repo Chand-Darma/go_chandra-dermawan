63fdd327e2f649da8bd9819947e471c4e7eb6717
Chand-Darma <chanddarmawan29@gmail.com>
2023-03-25 22:38:47 +0700

    push tugas 20 ke github

---
+++ 20_Intro Echo Golang/Praktikum/staticAPI_Echo.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+type User struct {
+	Id       int    `json:"id" form:"id"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
+
+var users []User
+
+// get all users
+func GetUsersController(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "success get all users",
+		"users":    users,
+	})
+}
+
+// get user by id
+func GetUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"messages": "invalid user id",
+		})
+	}
+	for _, user := range users {
+		if user.Id == id {
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				"messages": "success get user by id",
+				"user":     user,
+			})
+		}
+	}
+	return c.JSON(http.StatusNotFound, map[string]interface{}{
+		"messages": "user not found",
+	})
+}
+
+// delete user by id
+func DeleteUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"messages": "invalid user id",
+		})
+	}
+	for i, user := range users {
+		if user.Id == id {
+			users = append(users[:i], users[i+1:]...)
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				"messages": "success delete user",
+				"user":     user,
+			})
+		}
+	}
+	return c.JSON(http.StatusNotFound, map[string]interface{}{
+		"messages": "user not found",
+	})
+}
+
+// update user by id
+func UpdateUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"messages": "invalid user id",
+		})
+	}
+	for i, user := range users {
+		if user.Id == id {
+			// binding data
+			newUser := User{}
+			c.Bind(&newUser)
+			newUser.Id = user.Id
+			users[i] = newUser
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				"messages": "success update user",
+				"user":     newUser,
+			})
+		}
+	}
+	return c.JSON(http.StatusNotFound, map[string]interface{}{
+		"messages": "user not found",
+	})
+}
+
+// create new user
+func CreateUserController(c echo.Context) error {
+	// binding data
+	user := User{}
+	c.Bind(&user)
+
+	if len(users) == 0 {
+		user.Id = 1
+	} else {
+		newId := users[len(users)-1].Id + 1
+		user.Id = newId
+	}
+	users = append(users, user)
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "success create user",
+		"user":     user,
+	})
+}
+
+func main() {
+	e := echo.New()
+	// routing with query parameter
+	e.GET("/users", GetUsersController)
+	e.POST("/users", CreateUserController)
+	e.GET("/users/:id", GetUserController)
+	e.DELETE("/users/:id", DeleteUserController)
+	e.PUT("/users/:id", UpdateUserController)
+
+	// start the server, and log if it fails
+	e.Logger.Fatal(e.Start(":8000"))
+}
---
+++ 20_Intro Echo Golang/readme.md
@@ -0,0 +1,10 @@
+RINGKASAN INTRO ECHO GOLANG
+----------------------------
+
+1.  Dimateri ini saya belajar tentang `Konsep API`, `apa itu api`, `REST`, `JSON`, `HTTP Response Code`, `Open API`, `Introduction to POSTMAN`, `package net/http`.
+
+2.  belajar membuat api dan merancang staticAPI crud, ilustrasi API , hubungan antara FE dan BE, service to service, belajar REST juga , JSON cara memakai nya.
+
+3. belajar API Tools disini direkomendasikan nya POSTMAN, Format Response JSON, membuat , mengimport package, comsuming restful api, public api.
+
+4. belajar tentang framework Echo , kenapa harus memakai echo, memakai echo buat di api crud, mengatur basic routing dan controller, running echo, data rendering, serta retrieve data, `binding data`.
